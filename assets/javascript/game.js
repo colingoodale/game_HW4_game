38b4c9f10777cd61e9f4e7f8ca8d0e230fea6654
Colin <colingoodale@gmail.com>
2018-08-08 16:08:02 -0500

    onClick achieved

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var playerHealth = 100;
+
+var playerAttack = 6;
+
+var chosenEnemy = false;
+
+var counterAttack = 0;
+
+//choose attacker with click event
+
+pickEnemy.onClick();
+var chosenEnemy = id;
+console.log(chosenEnemy);
